886ff010765e809ba81b2c034942ea2f7152bbc7
github-username <dhwoqls1@gmail.com>
2023-07-27 15:57:43 +0900

    Initial Commit

---
+++ week2-TableProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2-TableProject
+//
+//  Created by Jae Oh on 2023/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
